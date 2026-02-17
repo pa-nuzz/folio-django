2a8f09d116fceadac0964be3e84ef854f4a629b4
pa-nuzz <94284238+pa-nuzz@users.noreply.github.com>
2026-02-17 17:05:29 +0545

    small change

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
-ALLOWED_HOSTS = ['yourdomain.com', 'www.yourdomain.com', 'localhost', '127.0.0.1','.ngrok-free.dev']
+ALLOWED_HOSTS = ['yourdomain.com', 'www.yourdomain.com', '*','localhost', '127.0.0.1','.ngrok-free.dev']
 
 
 # Application definition
